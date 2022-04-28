e893724fa9099a623cc6c1a36a9b98b724e847c8
Bryan <suprnewb@gmail.com>
2022-04-28 12:27:13 +1000

    Create computerPlay Function

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-Create function computerPlay
-    Return random Rock Paper or Scissors
+function computerPlay() {                 // Create function computerPlay
+    const arr = ["Rock", "Paper", "Scissors"];
+    const itemIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[itemIndex];
+    return item;                          // Return random Rock Paper or Scissors
+}
 
-Create function playRound that takes two parameters: playerSelection and computerSelection
-    Return string "You lose! X beats Y"
-playerSelection must be case insensitive
 
-Create function game()
-    Loop function playRound
-    console.log each round
-    return winner or loser after N games
-    prompt user for inputs
+// Create function playRound that takes two parameters: playerSelection and computerSelection
+//     Return string "You lose! X beats Y"
+// playerSelection must be case insensitive
+
+// Create function game()
+//     Loop function playRound
+//     console.log each round
+//     return winner or loser after N games
+//     prompt user for inputs
 
