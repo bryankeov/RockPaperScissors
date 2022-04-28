d4227ab8f948460aa4cce3fe416ea727f76de1d4
Bryan <suprnewb@gmail.com>
2022-04-28 11:23:49 +1000

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+Create function computerPlay
+    Return random Rock Paper or Scissors
+
+Create function playRound that takes two parameters: playerSelection and computerSelection
+    Return string "You lose! X beats Y"
+playerSelection must be case insensitive
+
+Create function game()
+    Loop function playRound
+    console.log each round
+    return winner or loser after N games
+    prompt user for inputs
+
